6e4be57558b572c8952db50117b25edfc8c00764
Entrar <Entrar>
2018-04-10 14:21:04 +0100

    First commit

---
+++ src/java/Product.java
@@ -0,0 +1,36 @@
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author entrar
+ */
+@XmlRootElement
+public class Product {
+    private String name;
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
---
+++ src/java/ProductResource.java
@@ -0,0 +1,40 @@
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author entrar
+ */
+
+@Stateless
+@Path("/product")
+
+public class ProductResource {
+    
+    static Product product;
+    
+    @GET
+    @Produces({"application/json", "application/xml"})
+    public Product leer(){
+        return product;
+    }
+    
+    @POST
+    @Consumes({"application/xml", "application/json"})
+    public Response guardar (Product p){
+        product = p;
+        return Response.ok(p).build();
+    }
+}
